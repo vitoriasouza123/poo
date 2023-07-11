3f95aa79f1796a8aa5b90fc6d2e7cbd8472c6f62
vitoriasouza123 <137115348+vitoriasouza123@users.noreply.github.com>
2023-07-11 17:52:59 -0300

    Pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -1,26 +1,17 @@
 public class Pessoa{
-	String nome;
-	int idade;
+	String nome, cpf;
 	char sexo;
-	String cpf;
-
-	Pessoa (String n, int i, char s, String c){
-		System.out.println ("Nova pessoa criada no sistema.");
+	Data dataNas;
+	
+	Pessoa (String n, String c, Data d, char s){
+		System.out.println ("\nNova pessoa criada no sistema.");
 		this.nome = n;
-		this.idade = i;
+		this.dataNas = d;
 		this.sexo = s;
 		this.cpf = c;
 	}
 
-	void aniversario (){
-		this.idade = this.idade+1;
-	}
 	void atribuiCPF (String c){
 		this.cpf = c ;
 	}
-	
-	int getIdade (){
-		return this.idade ;
-	}
-
 }
