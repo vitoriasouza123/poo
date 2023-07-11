c70e0e006ce61fafb91a75d5e9d6ad7f753fa0ba
vitoriasouza123 <137115348+vitoriasouza123@users.noreply.github.com>
2023-07-11 17:50:45 -0300

    Conta

--- Conta.java
+++ Conta.java
@@ -1,59 +1,24 @@
-public class Conta {
+public class Conta{
 	String numero;
 	Pessoa titular;
 	double saldo;
-	double limite;
-
-	Conta (String n, Pessoa t){
-		this.numero = n ;
-		this.titular = t ;
-		this.saldo = 0;
-		this.limite = 200;
-	}
-
-	Conta (String n, Pessoa t, double l){
-		this (n, t);
-		this.limite = l;
-	}
-
-	double disponivel () {
-		return this.saldo + this.limite;
+	Data criacao;
+	Gerente gerente;
+	
+	public Conta (String numero, Pessoa titular, Gerente g, Data c){
+		this.numero=numero;
+		this.titular=titular;
+		this.saldo=0;
+		this.criacao=c;
+		this.gerente=g;
 	}
 	
-	void extrato () {
-		System.out.println ("\n*** EXTRATO DA CONTA ***");
-		System.out.println ("Conta: " + this.numero);
-		System.out.println ("Titular: " + this.titular.cpf);
-		System.out.println ("Valor disponivel para saque: " +
-		this.disponivel () + "\n");
+	void depositar (double valor){
+		this.saldo+=valor;
+		System.out.println ("\nDeposito na conta " + this.numero + " realizado com sucesso. Novo saldo: " + this.saldo);
 	}
 	
-	void sacar (double valor) {
-		if (valor <= this.disponivel ()){
-			this.saldo -= valor;
-			System.out.println ("Saque de " + valor + " realizado com sucesso.");
-			System.out.println ("Novo saldo: " + this.saldo);
-		}else{
-			System.out.println ("Erro: nao foi possivel sacar " + valor);
-			System.out.println ("Valor disponivel para saque: " + this.disponivel ());
-		}
-	}
-	
-	void depositar (double valor) {
-		this.saldo += valor;
-		System.out.println ("Deposito de " + valor + " realizado com sucesso.");
-		System.out.println ("Novo saldo: " + this.saldo);
-	}
-	
-	void transferir (double valor , Conta destino) {
-		if (valor <= this.disponivel ()) {
-			this.sacar (valor);
-			destino.depositar (valor);
-			System.out.println ("Transferencia de " + valor + " realizado com sucesso.");
-			System.out.println ("Origem: " + this.numero + " / Destino: " + destino.numero);
-		}else{
-			System.out.println ("Erro: nao foi possivel transferir " + valor);
-			System.out.println ("Valor disponivel para transferencia: " + this.disponivel ());
-		}
+	public void extrato() {
+    System.out.println("Saldo: " + saldo + "\n");
 	}
 }
